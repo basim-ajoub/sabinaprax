16184d9bcd34a622a435ad41c8dafbcb09fd5cac
basim-ajoub <b_asim_star@hotmail.com>
2022-08-27 12:14:42 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,390 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg" id="navbar">
+      <div class="container nav-container">
+        <div class="container-fluid row">
+          <div class="logo-container col-md-12 col-lg-6">
+            <div class="row">
+              <div class="logo-name col-md-6">
+                <img class="img-logo" src="DAMAS-2.jpg" alt="image" />
+                <a class="navbar-brand" href="#">
+                  <span class="brand-name-1">SABINA</span><br />
+                  <span class="brand-name-2">Praxis</span>
+                </a>
+              </div>
+              <button
+                class="navbar-toggler btn-outline-success col-md-6"
+                data-bs-toggle="collapse"
+                data-bs-target="#nav-btn"
+                aria-controls="#nav-btn"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+            </div>
+          </div>
+          <div class="navbar-collapse collapse col-lg-6" id="nav-btn">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <router-link class="router-link" to="/">StartSeite</router-link>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#services"> Leistungen </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#featured-work"> Galarie </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#footer"> kontacte</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div id="slider" class="slider">
+      <div class="overlay"></div>
+      <transition-group class="img-container" name="fade" tag="div">
+        <div v-for="i in [currentIndex]" :key="i">
+          <img :src="currentImg" />
+          <h1 class="title-text">{{ currentTitle }}</h1>
+        </div>
+      </transition-group>
+      <a class="prev" @click="prev" href="#">&#10094; </a>
+      <a class="next" @click="next" href="#">&#10095; </a>
+    </div>
+    <!--Start Container-->
+    <router-view />
+    <!--End Container-->
+
+    <div id="footer" class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="company-info text-start">
+              <img src="DAMAS-2.jpg" alt="img-logo" />
+              <h2>
+                <a class="navbar-brand" href="#">
+                  <span class="brand-name-1">DAMAS</span><br />
+                  <span class="brand-name-2">3D Visiualisirung</span>
+                </a>
+              </h2>
+              <p>
+                Mithilfe neuester Technologien erhalten Sie bei DAMAS für Ihr
+                Neubauprojekt eine hochwertige 3D Visualisierung
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="map-container">
+              <h2>Our Location</h2>
+              <div class="location-window">
+                <iframe
+                  class="map"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.5853698147152!2d13.322581415807562!3d52.52284237981468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a8511118e4694b%3A0xe597423cfaceea3f!2sPascalstra%C3%9Fe%206%2C%2010587%20Berlin!5e0!3m2!1sen!2sde!4v1646951973111!5m2!1sen!2sde"
+                  style="border: 0"
+                  allowfullscreen=""
+                  loading="lazy"
+                ></iframe>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="contact-container">
+              <h2>Contact Us</h2>
+              <ul class="list">
+                <li>Adresse :<span>Pascalstraße 06. 10587 Berlin</span></li>
+                <li>Handy :<span>+ 49 (0) 331 704 901 39</span></li>
+                <li>Email :<span>info@damas.de</span></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      images: ["slide-1.jpg", "slide-2.jpg", "slide-3.jpg"],
+      title: ["Innenraumvisualisierung", "360° Panoramen", "3D Grundrisse"],
+      timer: null,
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    startSlide: function () {
+      this.timer = setInterval(this.next, 5000);
+    },
+
+    next: function () {
+      this.currentIndex += 1;
+    },
+    prev: function () {
+      this.currentIndex -= 1;
+    },
+  },
+  mounted() {
+    this.startSlide();
+    //to calculate window height to adjust the slider
+    var windoHT = window.innerHeight;
+    var NavBarHT = document.getElementById("navbar").offsetHeight;
+    var SliderHtT = windoHT - NavBarHT;
+    document.getElementById("carousel-inner").style.height = SliderHtT + "px";
+    document.getElementById("slider").style.height = SliderHtT + "px";
+    document.getElementById("carousel-item-1").style.height = SliderHtT + "px";
+    document.getElementById("carousel-item-2").style.height = SliderHtT + "px";
+    document.getElementById("carousel-item-3").style.height = SliderHtT + "px";
+    // end calculate window height to adjust the slider Dynamically
+  },
+
+  computed: {
+    currentImg: function () {
+      return this.images[Math.abs(this.currentIndex) % this.images.length];
+    },
+    currentTitle: function () {
+      return this.title[Math.abs(this.currentIndex) % this.title.length];
+    },
+  },
+};
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&family=News+Cycle&display=swap");
+</style>
+<style lang="scss">
+$blueCol: #2c3e50;
+$greenCol: #6d8b74;
+$LightgreenCol: #c4dfaa;
+$pinkCol: #ffadad;
+$whiteCol: #fdf8fa;
+$SkyCol: #c4dfaa;
+$fontSize: 4rem;
+$fontSizeTitle: 2rem;
+//for scroll bar Start
+/* Works on Firefox */
+* {
+  scrollbar-width: 8px;
+  scrollbar-color: $SkyCol $greenCol;
+  scroll-behavior: auto;
+}
+
+/* Works on Chrome, Edge, and Safari */
+*::-webkit-scrollbar {
+  width: 20px;
+}
+
+*::-webkit-scrollbar-track {
+  background: $greenCol;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: $SkyCol;
+  border-radius: 20px;
+  border: 3px solid $greenCol;
+}
+//End Scroll bar
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: #f6f6f6;
+  scroll-behavior: smooth;
+}
+.logo-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+}
+#slider {
+  //background: red;
+  position: relative;
+  font-family: "Bebas Neue", cursive;
+  letter-spacing: 0.15rem;
+  h1 {
+  }
+  .img-container {
+    width: 100%;
+    height: 100%;
+  }
+  .title-text {
+    max-width: 500px;
+    position: absolute;
+    z-index: 10;
+    top: 20%;
+    left: 6%;
+    color: $whiteCol;
+  }
+  .overlay {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 10;
+    background: rgba(0, 0, 0, 0.589);
+  }
+}
+.navbar {
+  padding: 30px;
+  background: #6d9886;
+  background: linear-gradient(to top left, $greenCol, $LightgreenCol);
+  border-bottom: 2.5px solid $SkyCol;
+
+  .nav-container {
+    max-width: 100%;
+  }
+  .navbar-toggler {
+    background: $SkyCol;
+  }
+  .navbar-nav {
+    text-transform: uppercase;
+    font-family: "News Cycle", sans-serif;
+    letter-spacing: 0.15rem;
+    .router-link {
+      font-weight: bold;
+      color: $SkyCol;
+      display: block;
+      padding: 0.5rem 1rem;
+      padding-right: 0.5rem;
+      padding-left: 0.5rem;
+      text-decoration: none;
+      transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+        border-color 0.15s ease-in-out;
+      &.router-link-exact-active {
+        color: $SkyCol;
+      }
+    }
+    a {
+      font-weight: bold;
+      color: $whiteCol;
+      text-decoration: none;
+      &.router-link-exact-active {
+        color: $SkyCol;
+      }
+    }
+    a,
+    .router-link {
+      font-size: 15px;
+      margin-left: 20px;
+    }
+  }
+}
+.navbar-brand {
+  text-align: left;
+  font-family: "Bebas Neue", cursive;
+  .brand-name-1 {
+    color: $whiteCol;
+    font-size: 2.1rem;
+    letter-spacing: 0.25rem;
+  }
+  .brand-name-2 {
+    color: $whiteCol;
+    font-size: 1.2rem;
+    letter-spacing: 0.15rem;
+  }
+  a {
+    text-decoration: none;
+  }
+}
+
+img {
+  height: 600px;
+  width: 100%;
+}
+.img-logo {
+  width: 5rem;
+  height: 5rem;
+  margin-right: 0.6rem;
+}
+.prev,
+.next {
+  cursor: pointer;
+  position: absolute;
+  top: 40%;
+  width: auto;
+  padding: 16px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.7s ease;
+  border-radius: 0 4px 4px 0;
+  text-decoration: none;
+  user-select: none;
+}
+
+.next {
+  right: 0;
+  z-index: 11;
+}
+
+.prev {
+  left: 0;
+  z-index: 11;
+}
+
+.prev:hover,
+.next:hover {
+  background-color: rgba(0, 0, 0, 0.507);
+}
+
+.footer {
+  background: $greenCol;
+  max-height: 120rem;
+  padding: 4rem 0rem;
+  p,
+  li {
+    color: $SkyCol;
+  }
+  h2 {
+    font-size: $fontSizeTitle;
+    color: $whiteCol;
+    font-family: "News Cycle", sans-serif;
+    letter-spacing: 0.1rem;
+  }
+  .company-info {
+    max-height: 70rem;
+    img {
+      width: 4rem;
+      height: 4rem;
+      margin-bottom: 1rem;
+    }
+    .navbar-brand {
+      font-size: 2rem;
+      .brand-name-2 {
+        font-size: 1.25rem;
+      }
+    }
+  }
+  .contact-container {
+    .list {
+      margin-top: 3rem;
+      margin-left: 1.5rem;
+      text-align: left;
+      list-style-type: none;
+    }
+  }
+  .map-container {
+    .location-window {
+      width: 100%;
+      height: 12rem;
+      .map {
+        width: 100%;
+        height: 100%;
+        margin-top: 1rem;
+        border-radius: 8%;
+      }
+    }
+  }
+}
+@media (max-width: 700px) {
+  .map-container,
+  .contact-container,
+  .company-info {
+    margin-bottom: 5rem;
+  }
+}
+</style>
---
+++ src/views/test.vue
@@ -0,0 +1,287 @@
+<template>
+  <div class="home">
+    <!--Start Features-->
+
+    <div id="services" class="features container">
+      <h2>Services</h2>
+      <p class="line"></p>
+      <div class="row">
+        <div class="service col-sm-5 col-lg-2">
+          <i class="rounded-circle">&#10094;</i>
+          <h3 class="service-title">Architekturvisualisierung</h3>
+        </div>
+        <div class="service col-sm-5 col-lg-2">
+          <i class="rounded-circle">&#10094;</i>
+          <h3 class="service-title">Innenraumvisualisierung</h3>
+        </div>
+        <div class="service col-sm-5 col-lg-2">
+          <i class="rounded-circle">&#10094;</i>
+          <h3 class="service-title">360° Panoramen</h3>
+        </div>
+        <div class="service col-sm-5 col-lg-2">
+          <i class="rounded-circle">&#10094;</i>
+          <h3 class="service-title">3D Grundrisse</h3>
+        </div>
+      </div>
+    </div>
+    <!--End Features-->
+
+    <!--Start OverView-->
+
+    <div id="overview" class="overview text-centir">
+      <h2>OverView</h2>
+      <p class="line"></p>
+      <div class="container">
+        <p>
+          Mithilfe neuester Technologien erhalten Sie bei DAMAS für Ihr
+          Neubauprojekt eine hochwertige 3D Visualisierung
+        </p>
+      </div>
+    </div>
+    <!--End OverView-->
+
+    <!--Start Featured Work-->
+    <div id="featured-work" class="featured-work">
+      <h2>Featured Work</h2>
+      <p class="line"></p>
+      <div class="containier">
+        <ul class="list row">
+          <li class="col-md active">All</li>
+          <li class="col-md">Berlin</li>
+          <li class="col-md">Stautgart</li>
+          <li class="col-md">Hamburg</li>
+        </ul>
+      </div>
+    </div>
+    <!--End Featured Work-->
+
+    <div class="projects-sec">
+      <div class="row">
+        <img class="col-lg" src="project-1.jpg" alt="" />
+        <img class="col-lg" src="project-2.jpg" alt="" />
+        <img class="col-lg" src="project-3.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-4.jpg" alt="" />
+        <img class="col-lg" src="project-5.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-9.jpg" alt="" />
+        <img class="col-lg" src="project-10.jpg" alt="" />
+        <img class="col-lg" src="project-11.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-7.jpg" alt="" />
+        <img class="col-lg" src="project-8.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-14.jpg" alt="" />
+        <img class="col-lg" src="project-13.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-17.jpg" alt="" />
+        <img class="col-lg" src="project-15.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-16.jpg" alt="" />
+        <img class="col-lg" src="project-12.jpg" alt="" />
+        <img class="col-lg" src="project-18.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-19.jpg" alt="" />
+        <img class="col-lg" src="project-20.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-21.jpg" alt="" />
+        <img class="col-lg" src="project-22.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-23.jpg" alt="" />
+        <img class="col-lg" src="project-24.jpg" alt="" />
+        <img class="col-lg" src="project-25.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-26.jpg" alt="" />
+        <img class="col-lg" src="project-27.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-28.jpg" alt="" />
+        <img class="col-lg" src="project-29.jpg" alt="" />
+        <img class="col-lg" src="project-30.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-31.jpg" alt="" />
+        <img class="col-lg" src="project-32.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-33.jpg" alt="" />
+        <img class="col-lg" src="project-34.jpg" alt="" />
+        <img class="col-lg" src="project-35.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-36.jpg" alt="" />
+        <img class="col-lg" src="project-37.jpg" alt="" />
+        <img class="col-lg" src="project-38.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-39.jpg" alt="" />
+        <img class="col-lg" src="project-40.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-41.jpg" alt="" />
+        <img class="col-lg" src="project-42.jpg" alt="" />
+        <img class="col-lg" src="project-43.jpg" alt="" />
+      </div>
+      <div class="row">
+        <img class="col-lg" src="project-44.jpg" alt="" />
+        <img class="col-lg" src="project-45.jpg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&family=News+Cycle&display=swap");
+</style>
+<style lang="scss">
+$blueCol: #2c3e50;
+$greenCol: #1b1b1b;
+$LightgreenCol: #181818;
+$pinkCol: #ffadad;
+$whiteCol: #fdf8fa;
+$SkyCol: #1cd6f4;
+$fontSize: 1rem;
+.home {
+  max-width: 99.1%;
+  text-align: center;
+  h2 {
+    margin-bottom: 3rem;
+    font-family: "News Cycle", sans-serif;
+    font-weight: bolder;
+  }
+  .line {
+    background: $SkyCol;
+    width: 4.5rem;
+    height: 0.19rem;
+    margin-top: -2.5%;
+    border-radius: 22%;
+    border-top-right-radius: 100%;
+    margin-left: 47.5%;
+    margin-bottom: 3rem;
+  }
+  .features {
+    margin-top: 100px;
+    max-width: 100%;
+    max-height: 120rem;
+  }
+}
+.container .row {
+  justify-content: space-evenly;
+}
+
+.service {
+  background: $greenCol;
+  margin-bottom: 50px;
+  padding: 15px;
+  cursor: pointer;
+  border-radius: 3%;
+
+  i {
+    background: $SkyCol;
+    color: $whiteCol;
+
+    padding: 5px 15px 5px 5px;
+    font-size: 30px;
+    margin-top: 70px;
+  }
+  h3,
+  p {
+    color: $whiteCol;
+  }
+  h3 {
+    margin-top: 15px;
+  }
+}
+.overview {
+  height: 300px;
+}
+.featured-work {
+  .list {
+    list-style-type: none;
+    padding: 100px 0px;
+    margin-left: 0.1rem;
+    margin-right: 0.1rem;
+    li {
+      padding: 10px;
+      border: 1px solid $blueCol;
+      margin-left: 10px;
+      cursor: pointer;
+      -webkit-transition: all 0.3s ease-in-out;
+
+      -moz-transition: all 0.3s ease-in-out;
+
+      -o-transition: all 0.3s ease-in-out;
+    }
+    li.active {
+      background: $greenCol;
+      color: $whiteCol;
+    }
+    li:hover {
+      color: $whiteCol;
+      background: $greenCol;
+    }
+  }
+}
+
+//Start Projects Section
+.service-title {
+  font-size: $fontSize;
+}
+.projects-sec {
+  img {
+    width: 20rem;
+    height: 20rem;
+    margin-bottom: 2rem;
+    padding-right: 0;
+    cursor: pointer;
+  }
+}
+@media (max-width: 900px) {
+  .home .line {
+    margin-left: 45.5%;
+  }
+  .featured-work {
+    .list {
+      margin-left: 0.05rem;
+      margin-right: 0rem;
+      li {
+        margin-left: 0;
+      }
+    }
+  }
+  .projects-sec {
+    img {
+      width: 26rem;
+    }
+  }
+}
+@media (max-width: 700px) {
+  .home .line {
+    margin-left: 41.5%;
+  }
+  .projects-sec {
+    img {
+      width: 100%;
+    }
+  }
+}
+
+//End Projects Section
+</style>
